3b52ae3cb8af18cca24546edf39f4a50b87284e9
Zyme <100279699+Htet-Wai-Yan-HWY@users.noreply.github.com>
2022-05-09 22:18:18 +0630

    Create temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+int main(){
+    signed int cel= -40;
+    float fah[9];
+
+    std::cout << " CELSIUS   FAHRENHEIT\n";
+    for (int i=0; i<=9; i++){
+        fah[i] = 1.8*cel + 32.0;
+            std::cout << std::setw(10) << cel << std::setw(14) << fah[i] << std::endl;
+            cel += 10;
+    }
+    return 0;
+}
